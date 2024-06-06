92ac6bc248f97fc42a9ced74e493a0ba6e525c34
hvtheer <hthe1010@gmail.com>
2024-06-06 14:41:55 +0700

    reset up base

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,37 @@
-// Copyright 2020 The Flutter Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:provider/provider.dart';
-import 'models/favorites.dart';
-import 'screens/favorites.dart';
-import 'screens/home.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:schedular_app/route_handler.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const TestingApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await dotenv.load(fileName: "assets/.env");
+  runApp(const MyApp());
 }
 
-final _router = GoRouter(
-  routes: [
-    GoRoute(
-      path: HomePage.routeName,
-      builder: (context, state) {
-        return const HomePage();
-      },
-      routes: [
-        GoRoute(
-          path: FavoritesPage.routeName,
-          builder: (context, state) {
-            return const FavoritesPage();
-          },
-        ),
-      ],
-    ),
-  ],
-);
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-class TestingApp extends StatelessWidget {
-  const TestingApp({super.key});
+  static const primaryColor = Colors.orange;
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  
+  @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<Favorites>(
-      create: (context) => Favorites(),
-      child: MaterialApp.router(
-        title: 'Testing Sample',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(
-            seedColor: Colors.deepPurple,
-          ),
-          useMaterial3: true,
-        ),
-        routerConfig: _router,
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: MyApp.primaryColor),
+      initialRoute: '/',
+      onGenerateRoute: RouteGenerator.generateRoute,
     );
   }
 }
